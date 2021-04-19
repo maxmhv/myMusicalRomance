73e7702e7cbe156f8588c2c068eeb04ae6010f75
Max Hovi <max.hovi@hotmail.fi>
2021-04-19 12:52:39 +0300

    Added image-file and added basic functionality

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-
+//Create dynamic "on submit"-listeners for the search bars, and save the inputted value to a variable
+var inputs = document.getElementsByClassName("inputSearch");
+for (i=0; i<inputs.length; i++){
+    inputs[i].addEventListener("submit", function (ev){
+        event.preventDefault();
+        if (ev.target.children[0].value == ""){
+            window.alert("Write something first!");
+        }
+        else{
+            var searchValue = ev.target.children[0].value;
+            console.log("Submitted value: "+ev.target.children[0].value);
+            ev.target.children[0].value ="";
+        }
+    })
+}
